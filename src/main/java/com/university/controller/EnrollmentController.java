dc3b7df277e58736bdcd9bb5e77fc63f97cba0e5
agustin <agus.maler@hotmail.com>
2021-03-03 19:28:21 -0300

    second commit

--- src/main/java/com/university/controller/EnrollmentController.java
+++ src/main/java/com/university/controller/EnrollmentController.java
@@ -1,4 +1,4 @@
-package com.university;
+package com.university.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
